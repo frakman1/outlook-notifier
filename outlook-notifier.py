d37ecfd8358e2c2d04bf8b4f12265ff14ad6ee44
Frak <frakman@hotmail.com>
2020-06-07 04:01:10 -0400

    Add audio notification and LIFX

--- outlook-notifier.py
+++ outlook-notifier.py
@@ -15,6 +15,9 @@ import win32com.client
 import sched, time
 from datetime import datetime, timedelta
 from colorama import init, Fore, Style
+import lazylights
+import binascii
+import playsound
 
 # global font variables
 bright_green = Fore.GREEN + Style.BRIGHT
@@ -24,7 +27,26 @@ bright_cyan = Fore.CYAN + Style.BRIGHT
 bright_red = Fore.RED + Style.BRIGHT
 bright_white = Fore.WHITE + Style.BRIGHT
 
-INTERVAL = 10
+INTERVAL = 10          # Polling Interval
+ALERT_TIME = 5         # Minutes before an event to alert you
+OFFICE_HOUR_START = 8  # 8AM
+OFFICE_HOUR_END = 18   # 6PM
+
+#------------------------------------------------------------------------------------------------------------
+# I use this to manually create a bulb using IP and MAC address. 
+def createBulb(ip, macString, port = 56700):        
+    return lazylights.Bulb(b'LIFXV2', binascii.unhexlify(macString.replace(':', '')), (ip,port))
+#------------------------------------------------------------------------------------------------------------	
+
+myBulb1 = createBulb('192.168.86.48','d0:73:d5:02:a9:1e')
+myBulb2 = createBulb('192.168.86.49','d0:73:d5:00:41:6d')
+myBulb3 = createBulb('192.168.86.62','d0:73:d5:20:ae:2f')
+myBulb4 = createBulb('192.168.86.79','d0:73:d5:2a:ce:e6')
+myBulb5 = createBulb('192.168.86.63','d0:73:d5:20:a0:00')
+myBulb6 = createBulb('192.168.86.47','d0:73:d5:02:6b:04')
+
+bulbs=[myBulb1, myBulb2, myBulb3, myBulb4, myBulb5, myBulb6]
+
 
 s = sched.scheduler(time.time, time.sleep)
 
@@ -65,21 +87,37 @@ def main_loop(sc):
     print(bright_magenta + "-----------------------------------------------------------------------------------------" + bright_white)
     print(bright_cyan + "📅️ {}".format(get_timestamp()) + bright_white)
 
-    events = getCalendarEntries()    
-    for event in events:
-        event_end = event['Start'] + timedelta(minutes=event['Duration'])
-        if ( event_end) < datetime.now():
-            print(bright_green + "    ✅ SAFE ✅      Event \"{}\" happened in the past (Started:{}. Ended:{})".format(event['Subject'], event['Start'].strftime("%I:%M %p"), event_end.strftime("%I:%M %p")) + bright_white)
-        if ( event['Start'] <= datetime.now() <= event_end):
-            print(bright_red + "    🚨 ALERT 🚨     Event \"{}\" is happening right now (Started {}. Ends {})".format(event['Subject'], event['Start'].strftime("%I:%M %p"), event_end.strftime("%I:%M %p")) + bright_white)
+    if  datetime.today().replace(hour=OFFICE_HOUR_START,minute=0) <= datetime.now() <= datetime.today().replace(hour=OFFICE_HOUR_END,minute=0):
+        print("Within office hours")
+        
+        events = getCalendarEntries()    
+        if not events:
+            print(bright_white + "     No events to report")
+            lazylights.set_state(bulbs, 0, 0, 1, 8000, 500, raw=False)  # white
         else:
-            alert_time = event['Start']-timedelta(minutes=5)
-            if alert_time <= datetime.now() <= event['Start']:
-                print(bright_yellow + "    ⏰ WARNING ⏰   Event \"{}\" starts within 5 minutes at {}".format(event['Subject'],event['Start'].strftime("%I:%M %p")) + bright_white)
-    
+            for event in events:
+                event_end  = event['Start'] + timedelta(minutes=event['Duration'])
+                alert_time = event['Start'] - timedelta(minutes=ALERT_TIME)
+                if ( event_end) < datetime.now():
+                    print(bright_green + "    ✅ SAFE ✅      Event \"{}\" happened in the past (Started:{}. Ended:{})".format(event['Subject'], event['Start'].strftime("%I:%M %p"), event_end.strftime("%I:%M %p")) + bright_white)
+                    lazylights.set_state(bulbs, 0, 0, 1, 8000, 500, raw=False)  # white
+
+                if ( event['Start'] <= datetime.now() <= event_end):
+                    print(bright_red + "    🚨 ALERT 🚨     Event \"{}\" is happening right now (Started {}. Ends {})".format(event['Subject'], event['Start'].strftime("%I:%M %p"), event_end.strftime("%I:%M %p")) + bright_white)
+                    lazylights.set_state(bulbs, 0, 1, 1, 8000, 500, raw=False)  # red
+                    
+                if alert_time <= datetime.now() <= event['Start']:
+                    print(bright_yellow + "    ⏰ WARNING ⏰   Event \"{}\" starts within 5 minutes at {}".format(event['Subject'],event['Start'].strftime("%I:%M %p")) + bright_white)
+                    playsound.playsound('UpcomingMeeting.mp3', True)
+                    lazylights.set_state(bulbs, 60, 1, 1, 8000, 500, raw=False)  # yellow
+                else:
+                    lazylights.set_state(bulbs, 0, 0, 1, 8000, 500, raw=False)  # white
+
     s.enter(INTERVAL, 1, main_loop, (sc,))
     
 def main():
+    
+
     s.enter(1, 1, main_loop, (s,))
     s.run()
     
